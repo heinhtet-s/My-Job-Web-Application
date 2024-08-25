9aad5b030995a6fd61672c062a6d3adfdb825145
Hein Htet Zaw <heinhtetzaw@viabells.com>
2024-08-25 11:54:58 +0700

    feedback fix

--- src/app/(Seeker)/(dashboard)/home/page.tsx
+++ src/app/(Seeker)/(dashboard)/home/page.tsx
@@ -5,6 +5,12 @@ const page = () => {
     <div>
       <h1 className="text-[38px] font-[700]">Welcome</h1>
       <p className="opactiy-70">Find your dream careers on Myjobs.com.mm</p>
+      <div
+        style={{
+          flex: "1 1 auto",
+          padding: "1rem 1rem",
+        }}
+      ></div>
     </div>
   );
 };
